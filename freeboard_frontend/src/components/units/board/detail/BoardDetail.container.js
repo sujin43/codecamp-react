655730d277723959a2469d829dd6e1a6fa48b775
devjnny <suuujj00@gmail.com>
2024-03-24 17:28:38 +0900

    ✏️react study: section11-map_과제3_게시물 삭제

--- freeboard_frontend/src/components/units/board/detail/BoardDetail.container.js
+++ freeboard_frontend/src/components/units/board/detail/BoardDetail.container.js
@@ -1,18 +1,28 @@
-import { useQuery } from "@apollo/client"
+import { useMutation, useQuery } from "@apollo/client"
 import { useRouter } from "next/router"
 import BoardDetailUI from "./BoardDetail.presenter"
 import { FETCH_BOARD } from "./BoardDetail.queries"
+import { DELETE_BOARD } from "./BoardDetail.queries"
 
 export default function BoardDetail() {
-    const router = useRouter()
-    const boardId = router.query.boardId
-    const { data } = useQuery(FETCH_BOARD, {
-        variables: {
-            boardId
-        }
+  const router = useRouter()
+  const boardId = router.query.boardId
+
+  const [deleteBoard] = useMutation(DELETE_BOARD)
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: {
+      boardId
+    }
+  })
+
+  const onClickDelete = () => {
+    const result = deleteBoard({
+      variables: {
+        boardId
+      }
     })
-    
-    return (
-        <BoardDetailUI data={data}/>
-    )
+    router.push("/boards")
+  }
+
+  return <BoardDetailUI data={data} onClickDelete={onClickDelete} />
 }
